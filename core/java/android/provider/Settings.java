91ee5be215d01b74072c55418c2b4d9e778efd6b
kldoc <fred.berbigier@gmail.com>
2020-11-05 11:21:50 -0300

    SystemUI: add Pulse and Ambient notification bars [1/2]
    [maxwen] SystemUI: support using notification color for pulse light
    
    optional use the color information of the notification
    as color for notification pulse light
    
    @brituj
    * Update color when a new notification arrives while pulsing
    * Fix row == null when on lockscreen even though the notification is clearable
    * other code improvements
    
    -------------------------------------------------------------------
    [SQUASH]
    Add Pulse and Ambient Display bars for notifications
    
    Change-Id: Ic5fdbdf05ef3f836e0c42ecef30e791a1f7eeb6e
    
    SystemUI: trigger pulse light only for notifications
    
    store the doze trigger reason in a setting to check later
    if it has been triggered for a notification or any other reason
    
    Change-Id: Iecee97b04e5e943c766fb4794d89e036ded65b3a
    
    SystemUI: Pulse light accent color option
    
    Change-Id: If402e96d7549aff7f758236e6f54a6034d08f9c2
    
    SystemUI: use ROWS_HIGH_PRIORITY for active notification pulse check
    
    only one of those could make pulse happen anyway
    
    Change-Id: I002344f321bdfed770f1099d7d33edc35671d997
    
    SystemUI: replace pulse light drawable with our own vector
    --------------------------------------------------------------------
    
    @markakash:
    * Fully Adapted for Android 12
    * Misc small changes
    
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>
    Change-Id: I95756d9886b2f4120d5ece72418850b100fe7394

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5520,6 +5520,24 @@ public final class Settings {
          */
 
         /**
+         * Whether to enable PULSE Edge lights
+         * @hide
+         */
+        public static final String AMBIENT_NOTIFICATION_LIGHT = "ambient_notification_light";
+
+        /**
+         * @hide
+         */
+        public static final String AMBIENT_NOTIFICATION_LIGHT_AUTOMATIC = "ambient_notification_light_automatic";
+
+        /**
+         * helper setting to contain the current pulse reason so we can check
+         * if it has been triggered from a notification or a gesture
+         * @hide
+         */
+        public static final String PULSE_TRIGGER_REASON = "pulse_trigger_reason";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5642,6 +5660,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
+            PRIVATE_SETTINGS.add(AMBIENT_NOTIFICATION_LIGHT);
+            PRIVATE_SETTINGS.add(AMBIENT_NOTIFICATION_LIGHT_AUTOMATIC);
         }
 
         /**
